cc5695ede60f51d7f94bad608b99214e786ed9cb
Olof Johansson <olof@ethup.se>
2020-01-22 14:56:59 +0100

    syslog: Focus on tail on start

--- gpd_ui/syslog.py
+++ gpd_ui/syslog.py
@@ -7,6 +7,7 @@ class SyslogView(View):
         lines = self.read_syslog()
         self._syslog = urwid.ListBox([urwid.Text(line.strip()) for line in lines])
         super().__init__(app=app, body=self._syslog)
+        self._syslog.set_focus(len(self._syslog.body)-1)
 
     def read_syslog(self):
         with open('/var/log/syslog') as fh:
